98ba30f85f12d3205af0054e294144a4c29b47f5
karma007 <abhishekpandey.kr@gmail.com>
2021-10-25 00:11:18 +0530

    head added for website

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Content from "../components/content";
 import { ContactMe } from "../components/ContactMe";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Abhishek Kumar Pandey | Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Content />
       <div className="gradient"></div>
       <ContactMe />
