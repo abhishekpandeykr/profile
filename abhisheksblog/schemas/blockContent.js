1ba12adbe3ad0cdf610d88125365a8547f6d38d5
karma007 <abhishekpandey.kr@gmail.com>
2022-01-01 23:01:52 +0530

    date handling logic for blog page

--- abhisheksblog/schemas/blockContent.js
+++ abhisheksblog/schemas/blockContent.js
@@ -1,3 +1,4 @@
+import BlockContent from "@sanity/block-content-to-react";
 /**
  * This is the schema definition for the rich text fields used for
  * for this blog studio. When you import it in schemas.js it can be
@@ -8,46 +9,68 @@
  *    type: 'blockContent'
  *  }
  */
-export default {
-  title: 'Block Content',
-  name: 'blockContent',
-  type: 'array',
+
+const BlockRenderer = (props) => {
+  const { style = "normal" } = props.node;
+
+  if (/^h\d/.test(style)) {
+    const level = style.replace(/[^\d]/g, "");
+    return React.createElement(
+      style,
+      { className: `heading-${level}` },
+      props.children
+    );
+  }
+
+  if (style === "blockquote") {
+    return <blockquote>- {props.children}</blockquote>;
+  }
+
+  // Fall back to default handling
+  return BlockContent.defaultSerializers.types.block(props);
+};
+const blockRender = {
+  title: "Block Content",
+  name: "blockContent",
+  type: "array",
   of: [
     {
-      title: 'Block',
-      type: 'block',
+      title: "Block",
+      type: "block",
       // Styles let you set what your user can mark up blocks with. These
       // correspond with HTML tags, but you can set any title or value
       // you want and decide how you want to deal with it where you want to
       // use your content.
       styles: [
-        {title: 'Normal', value: 'normal'},
-        {title: 'H1', value: 'h1'},
-        {title: 'H2', value: 'h2'},
-        {title: 'H3', value: 'h3'},
-        {title: 'H4', value: 'h4'},
-        {title: 'Quote', value: 'blockquote'},
+        { title: "Normal", value: "normal" },
+        { title: "H1", value: "h1" },
+        { title: "H2", value: "h2" },
+        { title: "H3", value: "h3" },
+        { title: "H4", value: "h4" },
+        { title: "Quote", value: "blockquote" },
+        { title: "Code", value: "code" },
       ],
-      lists: [{title: 'Bullet', value: 'bullet'}],
+      lists: [{ title: "Bullet", value: "bullet" }],
       // Marks let you mark up inline text in the block editor.
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
         // preference or highlighting by editors.
         decorators: [
-          {title: 'Strong', value: 'strong'},
-          {title: 'Emphasis', value: 'em'},
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
+          { title: "Code", value: "code" },
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
           {
-            title: 'URL',
-            name: 'link',
-            type: 'object',
+            title: "URL",
+            name: "link",
+            type: "object",
             fields: [
               {
-                title: 'URL',
-                name: 'href',
-                type: 'url',
+                title: "URL",
+                name: "href",
+                type: "url",
               },
             ],
           },
@@ -58,8 +81,10 @@ export default {
     // primitive types such as 'string' and 'number' in the same array
     // as a block type.
     {
-      type: 'image',
-      options: {hotspot: true},
+      type: "image",
+      options: { hotspot: true },
     },
   ],
-}
+};
+
+export default blockRender;
